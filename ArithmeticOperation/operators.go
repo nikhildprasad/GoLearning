87e659b01d4f83b71386ddb8a12e7fa620579324
nikhildprasad <nikhildprasad@gmail.com>
2026-01-14 03:35:28 +0530

    Basic arithmetic operation

---
+++ ArithmeticOperation/operators.go
@@ -0,0 +1,30 @@
+package arithmeticoperation
+
+import "fmt"
+
+func Add(a int, b int) int {
+	return a + b
+}
+
+func Subtract(a int, b int) int {
+	return a - b
+}
+
+func Multiply(a int, b int) int {
+	return a * b
+}
+
+func Divide(a int, b int) float64 {
+	if b == 0 {
+		return 0
+	}
+	return float64(a) / float64(b)
+}
+
+func main() {
+	fmt.Println("Arithmetic Operations:")
+	fmt.Println("Addition:", Add(5, 3))
+	fmt.Println("Subtraction:", Subtract(5, 3))
+	fmt.Println("Multiplication:", Multiply(5, 3))
+	fmt.Println("Division:", Divide(5, 3))
+}
